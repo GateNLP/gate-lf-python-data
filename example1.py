d531f8c8d9564d20b5dd751c2fb3b11139e2f766
Johann Petrak <johann.petrak@gmail.com>
2018-02-01 19:31:50 +0000

    Add example files for exploring and testing.

---
+++ example1.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from gatelfdata import Dataset
+from gatelfdata import Features
+import os
+
+ROOTDIR = os.path.join(os.path.dirname(__file__), '.')
+TESTDIR = os.path.join(ROOTDIR, 'tests')
+DATADIR = os.path.join(TESTDIR, 'data')
+
+TESTFILE1 = os.path.join(DATADIR, "class-ionosphere.meta.json")
+TESTFILE2 = os.path.join(DATADIR, "class-ngram-sp1.meta.json")
+TESTFILE3 = os.path.join(DATADIR, "class-window-pos1.meta.json")
+TESTFILE4 = os.path.join(DATADIR, "seq-pos1.meta.json")
+
+ds = Dataset(TESTFILE2)
+valset = ds.convert_to_file()
+for instance in ds.instances_as_data():
+  print("Instance: ",instance)
