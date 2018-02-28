b62e4baface63591beabfd9d3dec21bc94735129
Johann Petrak <johann.petrak@gmail.com>
2018-02-28 21:41:12 +0000

    Add documentation generation.

---
+++ gendoc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONPATH=`pwd`
+rm -rf ./doc-sphinx
+rm -rf ./doc-html
+mkdir doc-sphinx
+mkdir doc-html
+# cp sphinx-config/index.rst doc-sphinx
+sphinx-apidoc -e -f -H "GATE LF Python Data (gatelfdata)" -A "Johann Petrak" -V "0.1" -o doc-sphinx gatelfdata
+mv doc-sphinx/modules.rst doc-sphinx/index.rst
+sphinx-build -b html -c sphinx-config doc-sphinx doc-html 
---
+++ sphinx-config/conf.py
@@ -0,0 +1,22 @@
+# see PyTorch documentation for how this can be done
+
+master_doc = 'index'
+project = 'GATE LF Python-Data'
+copyright = '2018, University of Sheffield'
+author = 'Johann Petrak'
+
+version = '0.1'
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
