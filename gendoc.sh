7e4e226c5a8501dd258cd669f286867693e61130
Johann Petrak <johann.petrak@gmail.com>
2018-08-01 11:47:37 +0100

    Use plugins

--- gendoc.sh
+++ gendoc.sh
@@ -6,6 +6,6 @@ rm -rf ./doc-html
 mkdir doc-sphinx
 mkdir doc-html
 # cp sphinx-config/index.rst doc-sphinx
-sphinx-apidoc -e -f -H "GATE LF Python Data (gatelfdata)" -A "Johann Petrak" -V "0.1" -o doc-sphinx gatelfdata
+sphinx-apidoc -e -f -H "GATE LF Python Data (gatelfdata)" -A "Johann Petrak" -V "0.1" --ext-autodoc --ext-githubpages -o doc-sphinx gatelfdata
 mv doc-sphinx/modules.rst doc-sphinx/index.rst
 sphinx-build -b html -c sphinx-config doc-sphinx doc-html 
