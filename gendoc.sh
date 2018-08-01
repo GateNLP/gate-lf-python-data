2401994f19163694fbe7c6725a8498a996089e9a
Johann Petrak <johann.petrak@gmail.com>
2018-08-01 14:17:25 +0100

    Add generated python documentation

--- gendoc.sh
+++ gendoc.sh
@@ -9,3 +9,4 @@ mkdir doc-html
 sphinx-apidoc -e -f -H "GATE LF Python Data (gatelfdata)" -A "Johann Petrak" -V "0.1" --ext-autodoc --ext-githubpages -o doc-sphinx gatelfdata
 mv doc-sphinx/modules.rst doc-sphinx/index.rst
 sphinx-build -b html -c sphinx-config doc-sphinx doc-html 
+cp -r doc-html/* docs/pythondoc
