ad9b096127a000d851e916e0ef4178d54550aa72
Johann Petrak <johann.petrak@gmail.com>
2019-01-08 18:55:33 +0000

    Set version to 0.2

--- gatelfdata/__init__.py
+++ gatelfdata/__init__.py
@@ -1,4 +1,5 @@
 # https://stackoverflow.com/questions/10971033/backporting-python-3-openencoding-utf-8-to-python-2
+__version__ = '0.2'
 from gatelfdata.features import Features
 from gatelfdata.featureboolean import FeatureBoolean
 from gatelfdata.featurengram import FeatureNgram
